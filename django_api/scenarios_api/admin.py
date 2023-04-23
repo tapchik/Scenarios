2017ef4a4eeedec84f0958e0fc68d1b9be6b836e
Max <44063879+tapchik@users.noreply.github.com>
2023-04-23 21:49:13 +0500

    Registered models

--- django_api/scenarios_api/admin.py
+++ django_api/scenarios_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import TestSuite, TestCase, TestStep
 
 # Register your models here.
+
+admin.site.register(TestSuite)
+admin.site.register(TestCase)
+admin.site.register(TestStep)
